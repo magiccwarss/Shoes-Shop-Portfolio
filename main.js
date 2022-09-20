f177a7c524cdf007a550fc0c96c4bc8108e6086a
Narek Khachikyan <112387347+magiccwarss@users.noreply.github.com>
2022-09-20 19:18:11 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+const humbruger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav_menu");
+
+
+let menuBtn = document.querySelector('.burger');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+
+
+  AOS.init();
